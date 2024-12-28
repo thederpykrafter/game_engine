f15f52484c3a0a0c7c4271c4b4ff8232d85025bb
thederpykrafter <thederpykrafter@gmail.com>
2024-12-28 17:29:56 -0500

    create sdl2 window

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@
 CC = g++
 LANG = cpp
 CFLAGS = -g
+CLIBS = -lSDL2
 
 OUT_FILE = Hello
 
@@ -13,7 +14,7 @@ test: build run
 
 build: src/main.$(LANG)
 	@if [ ! -d bin ]; then mkdir bin; fi
-	$(CC) $(CFLAGS) src/main.$(LANG) -o bin/$(OUT_FILE)
+	$(CC) $(CFLAGS) src/main.$(LANG) $(CLIBS) -o bin/$(OUT_FILE)
 
 copy: bin/$(OUT_FILE)
 	cp -rf bin/$(OUT_FILE) /usr/bin
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
-using namespace std;
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
 
 int main() {
-	cout << "Hello, world!" << endl;
-	return 0;
+  SDL_Window *window = nullptr;
+  SDL_Renderer *renderer = nullptr;
+
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
+
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderDrawPoint(renderer, 640 / 2, 480 / 2);
+
+  SDL_RenderPresent(renderer);
+  SDL_Delay(10000);
+
+  return 0;
 }
