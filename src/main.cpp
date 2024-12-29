45acd6eca742cfe2ca32099308b68579f80a6f6e
thederpykrafter <thederpykrafter@gmail.com>
2024-12-28 20:20:14 -0500

    exit event

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,40 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
 
+using namespace std;
+
 int main() {
   SDL_Window *window = nullptr;
   SDL_Renderer *renderer = nullptr;
+  SDL_Event e;
 
-  SDL_Init(SDL_INIT_VIDEO);
+  // Init SDL
+  SDL_Init(SDL_INIT_EVERYTHING);
   SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
 
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_RenderClear(renderer);
+  bool running = true;
 
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  SDL_RenderDrawPoint(renderer, 640 / 2, 480 / 2);
+  while (running) {
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT)
+        running = false;
+    }
 
-  SDL_RenderPresent(renderer);
-  SDL_Delay(10000);
+    // Set color to clear
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
+    // Draw white pixel
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawPoint(renderer, 640 / 2, 480 / 2);
+
+    // Draw to screen
+    SDL_RenderPresent(renderer);
+    SDL_Delay(10000);
+  }
+  // Exit
   return 0;
 }
